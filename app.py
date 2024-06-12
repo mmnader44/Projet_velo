be9cc2e9f24a723580c36be37f19473e840bceb8
Glawui <167204011+Glawui@users.noreply.github.com>
2024-06-12 17:08:56 +0200

    tab created

--- app.py
+++ app.py
@@ -37,62 +37,46 @@ with st.sidebar:
         elif selected_type_service == "Location":
             df_final = df[df['Location'] == True]
 
-# Titrage
-st.markdown(
-    "<h2 style='color: black ; text-align: center;'>Services lié au vélo</h2>",
-    unsafe_allow_html=True,
-)
 #______________________Carte_____________________
 
-fig = px.scatter_mapbox(
-    df_final,
-    lat="latitude",
-    lon="longitude",
-    hover_name="Nom de l'enseigne",
-    hover_data={
-        "Adresse": True,
-        "Ville": True,
-        "Téléphone": True,
-        "Mail": True,
-        "Site internet": True,
-        "latitude": False,
-        "longitude": False,
-    },
-    zoom=10,)
-    
-
-fig.update_layout(
-    mapbox_style="carto-positron", width=800, height=650, showlegend=False
+# Onglets
+tab1, tab2 = st.tabs(
+    ["CARTE", "DATAVIZ"]
+)
+with tab1:
+    st.markdown(
+    "<h2 style='color: black ; text-align: center;'>Services lié au Vélo</h2>",
+    unsafe_allow_html=True,
+    )
+    fig = px.scatter_mapbox(
+        df_final,
+        lat="latitude",
+        lon="longitude",
+        hover_name="Nom de l'enseigne",
+        hover_data={
+            "Adresse": True,
+            "Ville": True,
+            "Téléphone": True,
+            "Mail": True,
+            "Site internet": True,
+            "latitude": False,
+            "longitude": False,
+        },
+        zoom=10,)
+    fig.update_layout(
+        mapbox_style="carto-positron", width=800, height=650, showlegend=False
     )
 
-    # Afficher la figure dans Streamlit
-st.plotly_chart(fig)
+        # Afficher la figure dans Streamlit
+    st.plotly_chart(fig)
+with tab2:
+    st.markdown(
+    "<h2 style='color: black ; text-align: center;'>Services lié au Vélo</h2>",
+    unsafe_allow_html=True,
+    )
+    df_count = df['Code postal'].value_counts().reset_index()
+    df_count.columns = ['Code postal', 'Nombre']
+    fig = px.bar(df_count, x='Code postal', y='Nombre', title='Nombre de valeurs par Code postal')
 
-# # création d'un DF pour l'affichage
-# columns_to_show = [
-#     "Nom de l'établissement",
-#     "Adresse de l'établissement",
-#     "email",
-#     "N° de téléphone",
-#     "Site Web",
-# ]
-# available_columns = [col for col in columns_to_show if col in df_filtered.columns]
-# df_show = df_filtered[available_columns]
-
-# if selected_etab == "Pas de sélection":
-#     st.write("Choisissez un  pour la recommandation")
-# else:
-#     st.write(
-#         f"""Vous avez choisi {df_reco.iloc[0]["Nom de l'établissement"]}\n
-#     L'adresse est le {df_reco.iloc[0]["Adresse de l'établissement"]}\n
-#     L'adresse mail est le {df_reco.iloc[0]["email"]}\n
-#     Joignable au {df_reco.iloc[0]["N° de téléphone"]}\n
-#     Site internet {df_reco.iloc[0]["Site Web"]}\n
-#     {df_reco.iloc[0]["information 1"]}\n
-#     {df_reco.iloc[0]["information 2"]}\n
-#     NaN signifie que la donnée n'a pas été entrée.\n
-#     \n
-#     \n
-#     Rendez-vous sur la page hébergement pour voir les hébergements les plus proches.
-#     """
-#     )
+    # Afficher le graphique
+    st.plotly_chart(fig)
