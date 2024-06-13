7108ceedc1b87a28edac7357547bdae57feb7259
Arkhankhamar <mm.nader44@gmail.com>
2024-06-13 11:19:53 +0200

    Modification du streamlit

--- app.py
+++ app.py
@@ -21,35 +21,59 @@ st.set_page_config(
 )
 
 #________________Sidebar_______________
+# Filtre dans la sidebar pour le type d'organisation et type de service
 with st.sidebar:
     st.write("Quels services cherchez-vous ?")
-    
+
+    # Filtre associatif
+    with st.expander("Type d'organisation :"):
+        orga_list = ['Tous', 'Associatif']
+        selected_orga_list = st.radio('Sélectionnez votre modèle :', orga_list)
+
     # Sélection du type de service
     with st.expander("Type de services"):
         type_service_list = ["Tous", "Ventes", "Location", "Réparation"]
-        selected_type_service = st.radio("Type de service :", type_service_list)
+        selected_type_service = st.radio("Sélectionnez votre service :", type_service_list)
 
-        # Filtrage du dataframe selon le type de service sélectionné
-        if selected_type_service == "Tous":
-            df_final = df
-        elif selected_type_service == "Ventes":
-            df_final = df[df['Ventes'] == True]
-        elif selected_type_service == "Réparation":
-            df_final = df[df['Réparation'] == True]
+# Filtrage du dataframe en fonction des choix faits dans la sidebar
+if selected_orga_list == "Tous" and selected_type_service == "Tous":
+    df_final = df  # Aucun filtrage appliqué
+elif selected_orga_list == "Tous":
+    # Filtrage uniquement par type de service
+    if selected_type_service == "Ventes":
+        df_final = df[df['Ventes'] == True]
+    elif selected_type_service == "Location":
+        df_final = df[df['Location'] == True]
+    elif selected_type_service == "Réparation":
+        df_final = df[df['Réparation'] == True]
+elif selected_orga_list == "Associatif":
+    # Filtrage par type d'organisation et éventuellement par type de service
+    df_final = df[df['Association'] == True]
+    
+    if selected_type_service != "Tous":
+        if selected_type_service == "Ventes":
+            df_final = df_final[df_final['Ventes'] == True]
         elif selected_type_service == "Location":
-            df_final = df[df['Location'] == True]
+            df_final = df_final[df_final['Location'] == True]
+        elif selected_type_service == "Réparation":
+            df_final = df_final[df_final['Réparation'] == True]
+else:
+    st.error('Sélection incorrecte')
+
 
 #______________________Carte_____________________
+
+st.markdown(
+"<h2 style='color: black ; text-align: center;'>Nantes Bike Map : Localisation des Services Vélo</h2>",
+unsafe_allow_html=True,
+)
 
 # Onglets
 tab1, tab2 = st.tabs(
     ["CARTE", "DATAVIZ"]
 )
 with tab1:
-    st.markdown(
-    "<h2 style='color: black ; text-align: center;'>Services lié au Vélo</h2>",
-    unsafe_allow_html=True,
-    )
+
     fig = px.scatter_mapbox(
         df_final,
         lat="latitude",
@@ -64,7 +88,8 @@ with tab1:
             "latitude": False,
             "longitude": False,
         },
-        zoom=10,).update_traces(marker={"size": 10})
+        zoom=10,
+        ).update_traces(marker={"size": 10})
     fig.update_layout(
         mapbox_style="carto-positron", width=1000, height=650, showlegend=False
     )
@@ -73,10 +98,6 @@ with tab1:
     st.plotly_chart(fig)
 
 with tab2:
-    st.markdown(
-    "<h2 style='color: black ; text-align: center;'>Services lié au Vélo</h2>",
-    unsafe_allow_html=True,
-    )
 
     # Sommes pour Mécanique
     somme_mecanique = df[['Vente de vélos neufs Classique Mécanique', 
